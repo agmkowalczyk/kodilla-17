95ec79917e02be80f9310dd5be89657cfc0a4aee
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-30 22:16:08 +0100

    kodilla 17.5

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('assets'));
+
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
 
 app.get('/', function (req, res) {
     res.sendFile('/index.html')
@@ -14,9 +19,14 @@ app.get('/userform', function (req, res) {
     res.end(JSON.stringify(response));
 });
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
+app.get('/store', function (req, res) {
+	res.send('To jest sklep');
+});
+
+const server = app.listen(3000, 'localhost', function() {
+    const host = server.address().address;
+    const port = server.address().port;
 
     console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
+
