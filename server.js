13c8aae2ef8f42f943675f84d8c4bc6d65b00ee7
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-11-30 20:43:28 +0100

    kodilla 17.4

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs')
 var app = express();
-var stringifyFile;
+app.use(express.static('assets'));
 
-app.use(bodyParser.json());
-
-app.get('/getNote', function(req, res) {
-
-	fs.readFile('./test.json', 'utf8', function(err, data) {
-	    if (err) throw err;
-	    stringifyFile = data
-	    res.send(data);
-	});
+app.get('/', function (req, res) {
+    res.sendFile('/index.html')
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    console.log('Otrzymałem żądanie POST');
-    stringifyFile = req.params.note;
-
-	fs.writeFile('./test.json', stringifyFile, function(err) {
-	    if (err) throw err;
-	    console.log('file updated');
-	});    
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-});
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-app.listen(3000);
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
