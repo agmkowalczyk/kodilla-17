bc64741a4aadb590cdf16a3272e8c18f7dc49c89
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-12-07 14:50:37 +0100

    kodilla 17.7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '830105699456-gmb5pocrgn0arn70cshi7qp7kk38i6op.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '6ZmdmI1kjb0l5Ev-Kd1g4o5o',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
